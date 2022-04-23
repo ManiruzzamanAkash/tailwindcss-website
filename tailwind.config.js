5d295aea15b2e588024e216375ea45da4635c128
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-04-23 14:33:29 +0600

    build: Removed unncessery libraries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
       },
       backgroundImage: {
-        'hero': "url('./../images/banner.png')",
-      },
+        'hero' : "url('./../images/banner.png')",
+      }
     },
   },
-  corePlugins: {
-    aspectRatio: false,
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ]
+  plugins: [],
 }
